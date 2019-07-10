ec4db7af8dc610fb6006054fa12145642ffb2bfb
Celal Cankaya <celalcankaya@outlook.com>
2019-07-10 14:16:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,18 @@
 class Dog
-  @name
+
   def name
     @name
   end
 
   def name=(nameNew)
     @name = nameNew
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(breedTypeNew)
+    @breed = breedTypeNew
   end
 end
