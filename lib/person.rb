78d46d3f0fc2086f872ac54169d38face2296904
Celal Cankaya <celalcankaya@outlook.com>
2019-07-10 14:13:13 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+  @nameA
+  def name
+    @nameA
+  end
+
+  def name=(nameNew)
+    @nameA = nameNew
+  end
+
+  @jobType
+  def job
+    @jobType
+  end
+
+  def job=(jobTypeNew)
+    @jobType = jobTypeNew
+  end
+end
